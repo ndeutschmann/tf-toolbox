c37db7e164438d4c244abc3eb1f88e569a1cb1de
Nicolas DEUTSCHMANN <>
2019-10-07 00:33:22 +0200

    added WIP piecewise linear

--- tf_toolbox/normalizing_flows.py
+++ tf_toolbox/normalizing_flows.py
@@ -32,3 +32,33 @@ class add_jacobian(keras.layers.Layer)           :
     
     def call(self,input):
         return tf.concat((input, tf.broadcast_to(self.jacobian_value,(input.shape[0],1)) ),axis=1)
+
+# WIP
+class PieceWiseLinear(keras.layers.Layer):
+    def __init__(self,n_bins,flow_size,pass_through_size,NN_layers):
+        super(NF_AffineCoupling,self).__init__()
+        self.pass_through_size = pass_through_size
+        self.flow_size = flow_size
+        self.transform_size = flow_size - pass_through_size
+        self.n_bins
+        sizes = NN_layers+[(n_bins*self.transform_size)]
+        NN_layers = [keras.layers.Dense(sizes[0],input_shape=(pass_through_size,),activation="relu")]
+        for size in sizes[1:-1]:
+            NN_layers.append(
+                keras.layers.Dense(size,activation="relu")
+            )
+
+        NN_layers.append(keras.layers.Dense(sizes[-1],activation="sigmoid"))
+        NN_layers.append(keras.layers.Reshape((self.transform_size,n_bins)))
+        self.NN = keras.Sequential(NN_layers)
+    
+    def call(self,input):
+        xA = input[:,:self.pass_through_size]
+        xB = input[:,self.pass_through_size:self.flow_size]
+        Q = self.NN(xA)
+        Qsum=tf.cumsum(Q,axis=-1)
+        alphas = xB*self.n_bins
+        bins = tf.math.floor(alphas)
+        alphas -= bins
+        bins = tf.cast(bins,tf.int32)
+        cdf_int_part =tf.gather(Qsum,tf.expand_dims(bins,axis=-1),batch_dims=-1,axis=2)
