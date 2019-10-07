tested and corrected jacobians for integration

tf_toolbox/normalizing_flows.py
@@ -65,7 +65,7 @@ class PieceWiseLinear(keras.layers.Layer):
         Q = self.NN(xA)
         Qsum = tf.cumsum(Q, axis=-1)
         Qnorms = tf.expand_dims(Qsum[:, :, -1], axis=-1)
-        Q /= Qnorms
+        Q /= Qnorms/self.n_bins
         Qsum /= Qnorms
         Qsum = tf.pad(Qsum,tf.constant([[0, 0], [0, 0], [1, 0]]))
         alphas = xB*self.n_bins
@@ -75,6 +75,6 @@ class PieceWiseLinear(keras.layers.Layer):
         # Sum of the integrals of the bins 
         cdf_int_part = tf.gather(Qsum, tf.expand_dims(bins, axis=-1), batch_dims=-1, axis=-1)
         cdf_float_part = tf.gather(Q, tf.expand_dims(bins, axis=-1), batch_dims=-1, axis=-1)
-        cdf = tf.reshape((cdf_float_part*tf.expand_dims(alphas, axis=-1))+cdf_int_part, cdf_int_part.shape[:-1])
+        cdf = tf.reshape((cdf_float_part*tf.expand_dims(alphas, axis=-1)/self.n_bins)+cdf_int_part, cdf_int_part.shape[:-1])
         jacobian *= tf.reduce_prod(cdf_float_part, axis=-2)
-        return tf.concat((cdf, jacobian), axis=-1)
+        return tf.concat((xA,cdf, jacobian), axis=-1)