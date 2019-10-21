934c0be353b2d0b7cb4149fd9ed9b54e7578f615
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2019-10-21 12:03:37 +0200

    Improved NF demo

--- NF_demo.py
+++ NF_demo.py
@@ -7,6 +7,7 @@
 import tensorflow as tf
 import tensorflow.keras as keras
 from tf_toolbox.normalizing_flows import *
+import matplotlib.pyplot as plt
 
 
 # In[2]:
@@ -23,18 +24,39 @@ def circle(x):
 n_flow = 2
 n_pass = 1
 n_bins = 10
-layer_size = 300
+layer_size = 20
 n_layers=10
 
 NF = NormalizingFlow(2,
     [
-        PieceWiseLinear(n_flow,n_pass,n_bins,[layer_size]*n_layers)
+        #---- Block
+        PieceWiseLinear(n_flow,n_pass,n_bins,[layer_size]*n_layers), #PW-linear coupling on variable 1
+        RollLayer(1), # Circular permutation of the variables (i.e. swap x1,x2)
+        PieceWiseLinear(n_flow,n_pass,n_bins,[layer_size]*n_layers), #PW-linear coupling on local variable 1 (i.e. x2)
+        RollLayer(1), # Circular permutation of the variables (i.e. swap x1,x2)
+        #---- End Block
     ])
 
 
-# In[ ]:
+# In[4]:
 
 
 optim = keras.optimizers.Adam(lr=1e-4)
-variances = NF.train_variance(circle,n_batch=10,optimizer=optim)
+variances = NF.train_variance(circle,n_epochs=70,n_steps=10,n_batch=100000,optimizer=optim)
+
+# In[5]:
+fig = plt.figure(figsize=(4, 8))
+fig.add_subplot(211)
+plt.plot(variances)
+X,fX=NF.generate_data_batches(circle)
+ax=fig.add_subplot(212)
+plt.hist2d(X[0][:,0],X[0][:,1])
+ax.set_aspect(aspect=1.)
+ax.tick_params(
+    axis='both',          # changes apply to the x-axis
+    which='both',      # both major and minor ticks are affected
+    bottom=False,      # ticks along the bottom edge are off
+    left=False,         # ticks along the top edge are off
+    labelbottom=False,
+    labelleft=False)
 
