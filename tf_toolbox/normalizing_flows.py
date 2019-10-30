cc9355056b43db02d90e53130f4847c724757d21
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2019-10-30 14:13:18 +0100

    re-added the implementation of eq 23 of 1808.03856

--- tf_toolbox/normalizing_flows.py
+++ tf_toolbox/normalizing_flows.py
@@ -223,7 +223,7 @@ class NormalizingFlow:
                 X = tf.stop_gradient(XJ[:,:-1])
                 J = XJ[:,-1]
                 fX = f(X)
-                loss = tf.reduce_mean(J*fX*tf.math.log(J+1e-3))
+                loss = tf.reduce_mean(J*fX*tf.math.log(J))
                 loss+=sum(self.inverse_model.losses)
             grads = tape.gradient(loss, self.model.trainable_variables)
             optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
@@ -249,6 +249,30 @@ class NormalizingFlow:
             if log_var:
                 var_log.append(loss)
             epoch_progress.set_description("Loss: {0:.3e} | Epoch".format(loss))
+        if log_var:
+            return(var_log)
+
+    def train_KL_backward(self,f ,n_batch = 10000,n_steps=1,n_epochs=10,*, optimizer,log_var=True):
+        if log_var:
+            var_log = []
+        epoch_progress = tqdm(range(n_epochs),leave=False,desc="Loss: {0:.3e} | Epoch".format(0.))
+        step_progress = tqdm_recycled(range(n_steps),desc="Step: ")
+        for i in epoch_progress:
+            # Generate some data
+            Xs,fXs = self.generate_data_batches(f,n_batch=n_batch,n_steps=n_steps)
+            for step in step_progress:
+                X = tf.stop_gradient(Xs[step])
+                fX = tf.stop_gradient(fXs[step])
+                with tf.GradientTape() as tape:
+                    J = self.inverse_model(self.format_input(X))[:, -1]
+                    loss = tf.reduce_mean(-fX / J * tf.math.log(J))
+                    loss += sum(self.inverse_model.losses)
+                grads = tape.gradient(loss, self.model.trainable_variables)
+                optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+                if log_var:
+                    var_log.append(loss)
+                epoch_progress.set_description("Loss: {0:.3e} | Epoch".format(loss))
+        step_progress.really_close()
         if log_var:
             return(var_log)
 
