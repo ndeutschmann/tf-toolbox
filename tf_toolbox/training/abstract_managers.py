42aff9a38b68637879673625094a41fed67af0ef
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2019-11-01 17:31:32 +0100

    put all abstract manager in a single submodule for easier imports

---
+++ tf_toolbox/training/abstract_managers.py
@@ -0,0 +1,5 @@
+"""Centralized collection of abstract manager classes"""
+
+from .optimizer_manager import OptimizerManager
+from .model_manager import ModelManager
+from .experiment_manager import ExperimentManager
