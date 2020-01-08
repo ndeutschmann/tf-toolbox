b7858768fb98a30374804c4b1f0bf9f6f401801f
Nicolas Deutschmann <ndeutschmann@hotmail.com>
2020-01-08 18:10:50 +0100

    implemented a maximally basic sacred experiment manager

--- tf_toolbox/training/managers.py
+++ tf_toolbox/training/managers.py
@@ -1,3 +1,5 @@
 from .tf_managers.optimizers import SGDManager, SGDMomentumManager, AdamManager, RMSpropManager
 from .tf_managers.models import DenseRectClassifierManager
 from .tf_managers.experiments import TBExperimentManager
+from .experiment_manager import LoggingExperimentManager
+from .sacred_managers.experiments import BasicSacredExperiment
--- tf_toolbox/training/sacred_managers/experiments.py
+++ tf_toolbox/training/sacred_managers/experiments.py
@@ -1,3 +1,5 @@
+import os
+from tensorboard.plugins.hparams import api as hp
 from sacred import Experiment
 from ..experiment_manager import LoggingExperimentManager
 
@@ -18,8 +20,28 @@ from ..experiment_manager import LoggingExperimentManager
 #   - generate the @main and @config functions automatically
 
 class BasicSacredExperiment(LoggingExperimentManager):
-    """TODO"""
-    pass
+    """Most basic experiment: take the sacred run object and manipulate it directly
+    See https://gist.github.com/ndeutschmann/b538c3b58cc4ae53a0e0e9df4c25ba85 v4 (08.01.2020) for usage
+    """
+    def start_model_manager_training(self, epoch_start=0, *, run , logdir, hparam, other_loggers=[], score_metric='loss', **runtime_options):
+
+        assert hasattr(self.model_manager,"save_hparams")
+        self.model_manager.save_hparams(hparam=hparam, logdir=logdir,prefix="")
+
+        # TODO:
+        # For now the call to the training function of LoggingExperimentManager is quite superfluous.
+        # However the idea is to have LoggingExperimentManager specify one of the user-facing API with detailed
+        # specifications on requirements.
+        history = super(BasicSacredExperiment, self).\
+            start_model_manager_training(logdir=logdir,
+                                         hparam=hparam,
+                                         epoch_start=epoch_start,
+                                         logger_functions=[run.log_scalar]+other_loggers,
+                                         **runtime_options)
+        run.add_artifact(os.path.join(logdir, "model_info", "hparams.yaml"))
+        if "save_best" in runtime_options and runtime_options['save_best']:
+            run.add_artifact(os.path.join(logdir, "model_info", "best", "weights.h5"))
+        return history.history[score_metric][-1]
 
 class SacredExperiment(LoggingExperimentManager):
     """TODO"""
